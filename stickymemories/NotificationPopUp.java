b772c3723480872ae57f55325b4621fcd76d0159
Daniel Simões <Daniel Simes@DESKTOP-JMSK7LV.wifi.isec.pt>
2016-12-05 23:21:11 +0000

    mais cenas

--- stickymemories/NotificationPopUp.java
+++ stickymemories/NotificationPopUp.java
@@ -11,7 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import stickymemories.core.Constants;
 import stickymemories.core.Resources;
-
+ 
 public class NotificationPopUp extends javax.swing.JFrame {
 
     private Image img = null;
